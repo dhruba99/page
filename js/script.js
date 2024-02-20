28fb152f411ce4dce59007e0c7766d7b8c8f3ef3
dhruba99 <153360398+dhruba99@users.noreply.github.com>
2024-02-20 23:16:59 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,202 @@
+ var imgName = [
+
+     //  {link: 'https://api.whatsapp.com/send?phone=918597140666&text=Hi,%20I%20want%20to%20join♥', src: '../img/1.jpg', name:'Sindhu', mobile: '+91-8597140666'},
+     //  {link: 'https://api.whatsapp.com/send?phone=918116529569&text=Hi,%20I%20want%20to%20join♥', src: '../img/2.jpg', name:'Latika', mobile: '+91-8116529569'},
+     {
+         link: 'https://api.whatsapp.com/send?phone=919083891738&text=Hi,%20I%20want%20to%20join♥♥',
+         src: '../img/3.jpg',
+         name: 'Madhu',
+         mobile: '+91-9083891738'
+     },
+     {
+         link: 'https://api.whatsapp.com/send?phone=918348669650&text=Hi,%20I%20want%20to%20join♥',
+         src: '../img/4.jpg',
+         name: 'Neha',
+         mobile: '+91-8348669650'
+     },
+     //  {link: 'https://api.whatsapp.com/send?phone=919339626411&text=Hi,%20I%20want%20to%20join♥', src: '../img/5.jpg', name:'Niharika', mobile: '+91-9339626411'},
+
+     {
+         link: 'https://api.whatsapp.com/send?phone=919083891738&text=Hi,%20I%20want%20to%20join♥♥',
+         src: '../img/6.jpg',
+         name: 'Riya',
+         mobile: '+91-9083891738'
+     },
+     {
+         link: 'https://api.whatsapp.com/send?phone=918348669650&text=Hi,%20I%20want%20to%20join♥♥',
+         src: '../img/7.jpg',
+         name: 'Sonali',
+         mobile: '+91-8348669650'
+     },
+     //  {link: 'https://api.whatsapp.com/send?phone=917501566915&text=Hi,%20I%20want%20to%20join♥', src: '../img/8.jpg', name:'Reena', mobile: '+91-7501566915'}, 
+     //  {link: 'https://api.whatsapp.com/send?phone=918597140666&text=Hi,%20I%20want%20to%20join♥', src: '../img/9.jpg', name:'Manisha', mobile: '+91-8597140666'},
+
+     //  {link: 'https://api.whatsapp.com/send?phone=917430839801&text=Hi,%20I%20want%20to%20join♥', src: '../img/10.jpg', name:'Malvika', mobile: '+91-7430839801'},
+     //    {link: 'https://api.whatsapp.com/send?phone=918346974053&text=Hi,%20I%20want%20to%20join♥', src: '../img/11.jpg', name:'Dimple', mobile: '+91-8346974053'},
+
+     //  {link: 'https://api.whatsapp.com/send?phone=918670575886&text=Hi,%20I%20want%20to%20join♥', src: '../img/12.jpg', name:'Nitica', mobile: '+91-8670575886'},
+     {
+         link: 'https://api.whatsapp.com/send?phone=917477364434&text=Hi,%20I%20want%20to%20join♥♥',
+         src: '../img/13.jpg',
+         name: 'Palak',
+         mobile: '+91-7477364434'
+     },
+     {
+         link: 'https://api.whatsapp.com/send?phone=916294773015&text=Hi,%20I%20want%20to%20join♥♥',
+         src: '../img/14.jpg',
+         name: 'Pallavi',
+         mobile: '+91-6294773015'
+     },
+     {
+         link: 'https://api.whatsapp.com/send?phone=918653096192&text=Hi,%20I%20want%20to%20join♥♥',
+         src: '../img/15.jpg',
+         name: 'Kanchan',
+         mobile: '+91-8653096192'
+     },
+
+     //  {link: 'https://api.whatsapp.com/send?phone=918536078029&text=Hi,%20I%20want%20to%20join♥', src: '../img/16.jpg', name:'Juhi', mobile: '+91-8536078029'}, 
+
+     //  {link: 'https://api.whatsapp.com/send?phone=919593840927&text=Hi,%20I%20want%20to%20join♥', src: '../img/17.jpg', name:'Shilpi', mobile: '+91-9593840927'},
+
+     //  {link: 'https://api.whatsapp.com/send?phone=917432937225&text=Hi,%20I%20want%20to%20join♥', src: '../img/18.jpg', name:'Dolly', mobile: '+91-7432937225'},
+     //  {link: 'https://api.whatsapp.com/send?phone=919593840927&text=Hi,%20I%20want%20to%20join♥', src: '../img/19.jpg', name:'Ruby', mobile: '+91-9593840927'},
+
+     {
+         link: 'https://api.whatsapp.com/send?phone=918293847103&text=Hi,%20I%20want%20to%20join♥♥',
+         src: '../img/20.jpg',
+         name: 'Shanya',
+         mobile: '+91-8293847103'
+     },
+
+     //  {link: 'https://api.whatsapp.com/send?phone=916295496016&text=Hi,%20I%20want%20to%20join♥', src: '../img/21.jpg', name:'Isha', mobile: '+91-6295496016'},
+     //  {link: 'https://api.whatsapp.com/send?phone=916295428461&text=Hi,%20I%20want%20to%20join♥', src: '../img/22.jpg', name:'Anvi', mobile: '+91-6295428461'},
+     //  {link: 'https://api.whatsapp.com/send?phone=919679616919&text=Hi,%20I%20want%20to%20join♥', src: '../img/23.jpg', name:'Neha', mobile: '+91-9679616919'},
+     //    {link: 'https://api.whatsapp.com/send?phone=919679449413&text=Hi,%20I%20want%20to%20join♥', src: '../img/24.jpg', name:'Rani', mobile: '+91-9679449413'},
+     //  {link: 'https://api.whatsapp.com/send?phone=919144624754&text=Hi,%20I%20want%20to%20join♥', src: '../img/25.jpg', name:'Ruhi', mobile: '+91-9144624754'},
+     //  {link: 'https://api.whatsapp.com/send?phone=917872598377&text=Hi,%20I%20want%20to%20join♥', src: '../img/26.jpg', name:'Monika', mobile: '+91-7872598377'},
+     //  {link: 'https://api.whatsapp.com/send?phone=917407834829&text=Hi,%20I%20want%20to%20join♥', src: '../img/27.jpg', name:'Madhu', mobile: '+91-7407834829'},
+     //  {link: 'https://api.whatsapp.com/send?phone=917432931728&text=Hi,%20I%20want%20to%20join♥', src: '../img/28.jpg', name:'Aditi', mobile: '+91-7432931728'},
+     //  {link: 'https://api.whatsapp.com/send?phone=918597140666&text=Hi,%20I%20want%20to%20join♥', src: '../img/29.jpg', name:'Manvi', mobile: '+91-8597140666'},
+     //  {link: 'https://api.whatsapp.com/send?phone=917384547487&text=Hi,%20I%20want%20to%20join♥', src: '../img/30.jpg', name:'Mousmi', mobile: '+91-7384547487'},
+     //    {link: 'https://api.whatsapp.com/send?phone=917501359914&text=Hi,%20I%20want%20to%20join♥♥', src: '../img/31.jpg', name:'Shruti', mobile: '+91-7501359914'},
+
+     {
+         link: 'https://api.whatsapp.com/send?phone=918972203482&text=Hi,%20I%20want%20to%20join♥♥',
+         src: '../img/32.jpg',
+         name: 'Ishita',
+         mobile: '+91-8972203482'
+     },
+     {
+         link: 'https://api.whatsapp.com/send?phone=917478931291&text=Hi,%20I%20want%20to%20join♥♥',
+         src: '../img/33.jpg',
+         name: 'Anjali',
+         mobile: '+91-7478931291'
+     },
+     {
+         link: 'https://api.whatsapp.com/send?phone=919046120878&text=Hi,%20I%20want%20to%20join♥♥',
+         src: '../img/34.jpg',
+         name: 'Ananya',
+         mobile: '+91-9046120878'
+     },
+     {
+         link: 'https://api.whatsapp.com/send?phone=916294773015&text=Hi,%20I%20want%20to%20join♥♥',
+         src: '../img/35.jpg',
+         name: 'Muskan',
+         mobile: '+91-6294773015'
+     },
+     //  {link: 'https://api.whatsapp.com/send?phone=918327690476&text=Hi,%20I%20want%20to%20join♥', src: '../img/36.jpg', name:'Nilima', mobile: '+91-8327690476'},
+     {
+         link: 'https://api.whatsapp.com/send?phone=918653096192&text=Hi,%20I%20want%20to%20join♥♥',
+         src: '../img/37.jpg',
+         name: 'Meghna',
+         mobile: '+91-8653096192'
+     },
+     {
+         link: 'https://api.whatsapp.com/send?phone=918371895606&text=Hi,%20I%20want%20to%20join♥♥',
+         src: '../img/38.jpg',
+         name: 'Afrine',
+         mobile: '+91-8371895606'
+     },
+     //  {link: 'https://api.whatsapp.com/send?phone=917478931260&text=Hi,%20I%20want%20to%20join♥', src: '../img/39.jpg', name:'Nandini', mobile: '+91-7478931260'},
+     //  {link: 'https://api.whatsapp.com/send?phone=918653096192&text=Hi,%20I%20want%20to%20join♥♥', src: '../img/40.jpg', name:'Natasha', mobile: '+91-8653096192'},
+     {
+         link: 'https://api.whatsapp.com/send?phone=918536997168&text=Hi,%20I%20want%20to%20join♥♥',
+         src: '../img/41.jpg',
+         name: 'Ankita',
+         mobile: '+91-8536997168'
+     },
+     {
+         link: 'https://api.whatsapp.com/send?phone=918695817622&text=Hi,%20I%20want%20to%20join♥♥',
+         src: '../img/42.jpg',
+         name: 'Arohi',
+         mobile: '+91-8695817622'
+     },
+     {
+         link: 'https://api.whatsapp.com/send?phone=918597305830&text=Hi,%20I%20want%20to%20join♥♥',
+         src: '../img/43.jpg',
+         name: 'Annu',
+         mobile: '+91-8597305830'
+     },
+     //  {link: 'https://api.whatsapp.com/send?phone=918514936876&text=Hi,%20I%20want%20to%20join♥♥', src: '../img/44.jpg', name:'Alisha', mobile: '+91-8514936876'},
+     //  {link: 'https://api.whatsapp.com/send?phone=918371895606&text=Hi,%20I%20want%20to%20join♥', src: '../img/45.jpg', name:'Somiya', mobile: '+91-8371895606'},
+     {
+         link: 'https://api.whatsapp.com/send?phone=919339626411&text=Hi,%20I%20want%20to%20join♥',
+         src: '../img/46.jpg',
+         name: 'Mohini',
+         mobile: '+91-9339626411'
+     },
+
+     //  {link: 'https://api.whatsapp.com/send?phone=917478932406&text=Hi,%20I%20want%20to%20join♥', src: '../img/42.jpg', name:'Arohi', mobile: '+91-7478932406'},
+     //  {link: 'https://api.whatsapp.com/send?phone=917478932406&text=Hi,%20I%20want%20to%20join♥', src: '../img/43.jpg', name:'Annu', mobile: '+91-7478932406'},
+     //  {link: 'https://api.whatsapp.com/send?phone=917478932406&text=Hi,%20I%20want%20to%20join♥', src: '../img/44.jpg', name:'Alisha', mobile: '+91-7478932406'},
+     //  {link: 'https://api.whatsapp.com/send?phone=917478932406&text=Hi,%20I%20want%20to%20join♥', src: '../img/45.jpg', name:'Somiya', mobile: '+91-7478932406'},
+     //  {link: 'https://api.whatsapp.com/send?phone=919339626411&text=Hi,%20I%20want%20to%20join♥', src: '../img/46.jpg', name:'Roshni', mobile: '+91-9339626411'},
+     {
+         link: 'https://api.whatsapp.com/send?phone=918016605738&text=Hi,%20I%20want%20to%20join♥♥',
+         src: '../img/21.jpg',
+         name: 'Tanya',
+         mobile: '+91-8016605738'
+     },
+     //  {link: 'https://api.whatsapp.com/send?phone=917501359914&text=Hi,%20I%20want%20to%20join♥♥', src: '../img/25.jpg', name:'Janvi', mobile: '+91-7501359914'},
+
+     {
+         link: 'https://api.whatsapp.com/send?phone=917365873809&text=Hi,%20I%20want%20to%20join♥♥',
+         src: '../img/24.jpg',
+         name: 'Sona',
+         mobile: '+91-7365873809'
+     },
+
+ ];
+
+
+ /*
+
+
+     {link: 'https://api.whatsapp.com/send?phone=918609781607&text=Do%20you%20believe%20in%20love%20at%20first%20sight?', src: '../img/19.jpg', name:'Karishma', mobile: '+91-8609781607'},
+     {link: 'https://api.whatsapp.com/send?phone=918514971425&text=Do%20you%20believe%20in%20love%20at%20first%20sight?', src: '../img/14.jpg', name:'Radhika', mobile: '+91-8514971425' },
+     {link: 'https://api.whatsapp.com/send?phone=918514974505&text=Do%20you%20believe%20in%20love%20at%20first%20sight?', src: '../img/15.jpg', name:'Saloni', mobile: '+91-8514974505'},
+     {link: 'https://api.whatsapp.com/send?phone=918436982662&text=Do%20you%20believe%20in%20love%20at%20first%20sight?', src: '../img/18.jpg', name:'Peehu', mobile: '+91-8436982662'},
+     {link: 'https://api.whatsapp.com/send?phone=918597069443&text=Do%20you%20believe%20in%20love%20at%20first%20sight?', src: '../img/19.jpg', name:'Prerna', mobile: '+91-8597069443'},
+     {link: 'https://api.whatsapp.com/send?phone=918512968954&text=Do%20you%20believe%20in%20love%20at%20first%20sight?', src: '../img/21.jpg', name:'Nidhi', mobile: '+91-8512968954'},
+        
+ */
+
+
+ $(document).ready(function() {
+     var ss = '<div class="col-md-3 col-6 ss"><center>'
+     var ss1 = 'class="img-fluid img-thumbnail rounded" alt="a1"><p class="text-dark">'
+     var ss2 = '</p></a></center></div>'
+     var newList = imgName.sort(() => .5 - Math.random());
+     for (i = 0; i < 20; i++) {
+         var ch = ss + '<a href="' + imgName[i].link + '"><img src="img/' + newList[i].src + '"' + ss1 + newList[i].name + ss2;
+         $(".imgAppend").append(ch);
+     }
+
+     setInterval(function() {
+         $('.imgAppend .ss').remove();
+         var newList = imgName.sort(() => .5 - Math.random());
+         for (i = 0; i < 20; i++) {
+             $(".imgAppend").append(ss + '<a href="' + imgName[i].link + '"><img src="img/' + newList[i].src + '"' + ss1 + newList[i].name + ss2);
+         }
+     }, 7000);
+ })
